b03aded1d39d81d414490656ef96e1b71b6abc76
adilraees786 <adilraees911@gmail.com>
2025-03-05 16:31:18 +0500

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className=' text-2xl text-red-500'>My App</h1>
+    <div>
+      <h1>My Meterial Ui</h1>
+    </div>
     </div>
   )
 }
