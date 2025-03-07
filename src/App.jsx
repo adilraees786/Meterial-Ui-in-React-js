b8874432dbb332122d826076cba42c6addd209a8
adilraees786 <adilraees911@gmail.com>
2025-03-07 16:09:38 +0500

    Add Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-import ReactDOM from "react-dom/client";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoPage from "./Pages/NoPage";
 import Home from "./Pages/Home";
 import Layout from './Pages/Layout';
 import Blogs from './Pages/Blogs';
 import Contact from "./Pages/Contact";
+import Signup from "./Components/Signup";
+
 
 
 
@@ -18,6 +20,7 @@ export default function App() {
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
+          <Route path="signup" element={<Signup />} />
         </Route>
       </Routes>
     </BrowserRouter>
